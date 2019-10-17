1f3e0fb8b42c63f4b275820d51c539bdd3d9e5a0
Admin <admin@AdmindeiMac-3.local>
2019-10-17 15:38:03 +0800

    Clear Demo

--- app/src/main/java/com/demo/ExampleApplication.java
+++ app/src/main/java/com/demo/ExampleApplication.java
@@ -2,13 +2,10 @@ package com.demo;
 
 import android.app.Application;
 
-import cn.jpush.android.api.JPushInterface;
 
 public class ExampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        JPushInterface.setDebugMode(true);
-        JPushInterface.init(this);
     }
 }
--- app/src/main/java/com/demo/PushService.java
+++
@@ -1,8 +0,0 @@
-package com.demo;
-
-import android.app.Service;
-
-import cn.jpush.android.service.JCommonService;
-
-public class PushService extends JCommonService {
-}
