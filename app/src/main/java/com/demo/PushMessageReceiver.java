ac73c371605263c6d813344932c7cfa75210928b
Admin <admin@AdmindeiMac-3.local>
2019-10-17 15:22:08 +0800

    first commit

---
+++ app/src/main/java/com/demo/PushMessageReceiver.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+import android.content.BroadcastReceiver;
+
+import cn.jpush.android.service.JPushMessageReceiver;
+
+public class PushMessageReceiver extends JPushMessageReceiver {
+}
---
+++ app/src/main/java/com/demo/PushService.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+import android.app.Service;
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+}
